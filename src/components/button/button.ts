b0483c5e42777df161bc43df71afb2e2226fe77f
Alexander King <alexanderddking@gmail.com>
2019-04-07 10:41:31 +0900

    Added Concurrently

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -3,8 +3,20 @@ import { LitElement, html, property, customElement } from 'lit-element';
 @customElement('r-button')
 export class SimpleGreeting extends LitElement {
   @property() name = 'World';
+  renderStyles(){
+    return html`
+    <style>
+      button {
+        background: hotpink;
+      }
+    </style>
+    `;
 
+  }
   render() {
-    return html`<p>Hello, ${this.name}!</p>`;
+    return html`
+      ${this.renderStyles()}
+      <button>Hello, ${this.name}!</button>
+    `;
   }
 }
