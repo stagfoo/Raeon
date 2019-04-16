6cb76936f7bdf74504c8cc99912f2b2d4d5bbc88
Alex King <stagfoo@users.noreply.github.com>
2019-04-16 09:47:16 +0900

    Update 0-intro.stories.ts

--- docs/stories/0-intro.stories.ts
+++ docs/stories/0-intro.stories.ts
@@ -1,6 +1,18 @@
 import { storiesOf } from '@storybook/html';
 storiesOf('Design System', module)
-  .add('Colors', () => {
-    return `<iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D107%253A90" allowfullscreen></iframe>`
+  .add('Core Colors', () => {
+    return `<iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D787%253A627" allowfullscreen></iframe>`
+})
+ .add('Dark Leve 1 Colors', () => {
+    return `<iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D787%253A627" allowfullscreen></iframe>`
+})
+ .add('Light Level 1 Colors', () => {
+    return `<iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D787%253A627" allowfullscreen></iframe>`
+})
+ .add('Light Level 2 Colors', () => {
+    return `<iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D787%253A627" allowfullscreen></iframe>`
+})
+ .add('Core Gradients', () => {
+    return `<iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D787%253A627" allowfullscreen></iframe>`
 })
 .add('Type', () => `<iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D6%253A2" allowfullscreen></iframe>`)
