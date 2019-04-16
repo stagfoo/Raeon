4deea82df67a9ad19dd00a6142b1568a5821d2c9
Alex King <alexanderddking@gmail.com>
2019-04-16 10:10:44 +0900

    New figma

--- docs/stories/0-intro.stories.ts
+++ docs/stories/0-intro.stories.ts
@@ -1,18 +1,18 @@
 import { storiesOf } from '@storybook/html';
 storiesOf('Design System', module)
-  .add('Core Colors', () => {
-    return `<iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D787%253A627" allowfullscreen></iframe>`
+  .add('Colors - Core', () => {
+    return `<iframe class="figma-frame" style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D787%253A627" allowfullscreen></iframe>`
 })
- .add('Dark Leve 1 Colors', () => {
-    return `<iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D787%253A627" allowfullscreen></iframe>`
+ .add('Colors - Dark Level 1', () => {
+    return `<iframe class="figma-frame" style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D814%253A705" allowfullscreen></iframe>`
 })
- .add('Light Level 1 Colors', () => {
-    return `<iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D787%253A627" allowfullscreen></iframe>`
+ .add('Colors - Light Level 1 ', () => {
+    return `<iframe class="figma-frame" style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D861%253A0" allowfullscreen></iframe>`
 })
- .add('Light Level 2 Colors', () => {
-    return `<iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D787%253A627" allowfullscreen></iframe>`
+ .add('Colors - Light Level 2 ', () => {
+    return `<iframe class="figma-frame" style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D861%253A50" allowfullscreen></iframe>`
 })
- .add('Core Gradients', () => {
-    return `<iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D787%253A627" allowfullscreen></iframe>`
+ .add('Gradients - Core', () => {
+    return `<iframe class="figma-frame" style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D814%253A755" allowfullscreen></iframe>`
 })
 .add('Type', () => `<iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFpKGFJhA3XsT0GNMVzA0Ywww%2FRaeon%3Fnode-id%3D6%253A2" allowfullscreen></iframe>`)
